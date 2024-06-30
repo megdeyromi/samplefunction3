3cc56b44bf51654a1120a4b04479ecdb550c27fc
meghalee_d <meghalee.dey@oracle.com>
2024-06-30 18:49:26 +0000

    2nd commit

--- func.py
+++ func.py
@@ -9,6 +9,26 @@ import io
 import json
 
 from fdk import response
+import io
+import json
+import pandas as pd
+import json
+import os
+import sys, getopt
+import pandas as pd
+import cohere
+import time
+import requests
+#API_KEY="ZXCkVLN1x6RfnMM19TBCJQ0Jki2Vq0OouTgkSGzf"
+API_KEY="B6ErOdasD4G2oYZyyM1xJFx8PwKRfDBhwiVmNRmB"
+
+co = cohere.Client(API_KEY)
+
+from fdk import response
+
+def downloadSwagger(url, file_path):
+    r = requests.get(url, allow_redirects=True)
+    open(file_path, 'wb').write(r.content)
 
 
 def handler(ctx, data: io.BytesIO=None):
